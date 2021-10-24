a7c7eea7dbd84bb6f19b0625a525625ce5da04f2
feLycks <felix.obiero@student.moringaschool.com>
2021-10-24 05:05:03 +0300

    middleware build

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express()
 
+const fortunes = require('./data/fortunes.json')
+const port = 3000
 
-app.listen(3000, () => console.log('Server Running'))
+app.use(bodyParser.json());
+
+// get all fortunes 
+app.get('/fortunes', (req, res) => res.send(fortunes))
+    // get fortunes by id 
+app.get('/fortunes/:id', (req, res) => {
+    res.send(fortunes.find(c => c.id == req.params.id))
+})
+
+// add fortunes 
+app.post('/fortunes', (req, res) => {
+
+})
+
+app.listen(port, () => console.log(`Server runnig on port ${port}`))
